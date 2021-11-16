b8e99a54e46b35410fe580c56ea517958b8c29de
AtifWeb <mytechlife96@gmail.com>
2021-11-16 07:52:46 -0800

    how it works css added

--- src/App/Components/common/Organizer/LocalHeader/LocalHeader.jsx
+++ src/App/Components/common/Organizer/LocalHeader/LocalHeader.jsx
@@ -3,32 +3,38 @@ import { Link } from "react-router-dom";
 
 import styles from "./LocalHeader.module.css";
 export const LocalHeader = ({ list, active }) => {
-  const HandleDropDown = (e, value) => {
-    if (value == "dropdown") {
-      e.preventDefault();
-      document.querySelector(".local-header").classList.toggle("dropdown");
-    }
+  const HandleDropDown = () => {
+    document.querySelector(".local-header").classList.toggle("dropdown");
   };
   return (
     <div className={`${styles.LocalHeader} local-header  `}>
       <ul className={`w-1000 ${styles.list_wrapper}`}>
         {list.map((Item) => (
           <li>
-            <Link
-              to={Item.redirection}
-              onClick={(e) => HandleDropDown(e, Item.value)}
-              className={`${styles.item_link} ${
-                active === Item.value && styles.active
-              }
+            {Item.value === "dropdown" && (
+              <div
+                onClick={(e) => HandleDropDown()}
+                className={`${styles.item_link} ${
+                  active === Item.value && styles.active
+                }`}
+              >
+                {active}
+                <i class={`fas fa-chevron-down ${styles.arrow}`}></i>
+              </div>
+            )}
+            {Item.value !== "dropdown" && (
+              <Link
+                to={Item.redirection}
+                className={`${styles.item_link} ${
+                  active === Item.value && styles.active
+                }
               
               
               `}
-            >
-              {Item.value === "dropdown" ? active : Item.value}
-              {Item.value === "dropdown" && (
-                <i class={`fas fa-chevron-down ${styles.arrow}`}></i>
-              )}
-            </Link>
+              >
+                {Item.value}
+              </Link>
+            )}
           </li>
         ))}
       </ul>
